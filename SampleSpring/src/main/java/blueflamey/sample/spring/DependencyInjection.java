15d46367ad1fa5793a5882572556a5a883e8c2c6
Blueflamey <neonblueflame@gmail.com>
2016-05-19 15:42:54 +0800

    First commit for sampleSpring

---
+++ SampleSpring/src/main/java/blueflamey/sample/spring/DependencyInjection.java
@@ -0,0 +1,12 @@
+package blueflamey.sample.spring;
+
+public class DependencyInjection {
+	public static void main(String[] args) {
+		/* TODO
+		 * https://projects.spring.io/spring-framework/
+		 * https://en.wikipedia.org/wiki/Spring_Framework
+		 * https://projects.spring.io/spring-framework/
+		 * https://en.wikipedia.org/wiki/Reflection_(computer_programming)
+		 */
+	}
+}
