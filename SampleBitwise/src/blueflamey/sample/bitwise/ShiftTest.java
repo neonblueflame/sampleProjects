41c790d3a0b87d5e9c4b549a4c4677834e560669
smariano <smariano@MAK-F1J7GY1.gemalto.com>
2017-03-09 17:43:32 +0800

    Initial commit for SampleBitwise

---
+++ SampleBitwise/src/blueflamey/sample/bitwise/ShiftTest.java
@@ -0,0 +1,46 @@
+package blueflamey.sample.bitwise;
+
+/**
+ * Uses Java 8 
+ */
+public class ShiftTest {
+	public static void main(String... args) {
+		
+		final int binaryZero = 0b0000_0000;
+		System.out.println(binaryZero);
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+		final int resultWhenLeftShiftDoneToZero = binaryZero << 1;
+		System.out.println(resultWhenLeftShiftDoneToZero);
+		
+		final int binaryOne = 0b0000_0001;
+		System.out.println("bianry one: " + binaryOne);
+		
+		final int resultWhenLeftShiftDoneToOne = binaryOne << 1;
+		System.out.println(resultWhenLeftShiftDoneToOne);
+		
+		final int resultWhenThreeLeftShiftDoneToOne = binaryOne << 3;
+		System.out.println(resultWhenThreeLeftShiftDoneToOne);
+		
+		final int resultWhenMaxLeftShiftDoneToOne = binaryOne << Integer.MAX_VALUE;
+		System.out.println(resultWhenMaxLeftShiftDoneToOne);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Integer.MIN_VALUE & resultWhenMaxLeftShiftDoneToOne);
+		
+		final int resultWhenMinValueLeftShiftOne = Integer.MIN_VALUE << 1;
+		System.out.println(resultWhenMinValueLeftShiftOne);
+		
+		final int resultWhenNegativeRightShiftDoneToOne = binaryOne >> -1;
+		System.out.println(resultWhenNegativeRightShiftDoneToOne);
+		
+		final int resultWhenLargeNumberRightShiftDoneToOne = binaryOne >> (Integer.MAX_VALUE);
+		System.out.println(resultWhenLargeNumberRightShiftDoneToOne);
+		
+		// Niko stuff
+		System.out.println("Niko stuff");
+		final int resultWhenRightShiftDoneToOne = ~-2 + 1;
+		System.out.println(resultWhenRightShiftDoneToOne + " == 2");
+	} 
+}
