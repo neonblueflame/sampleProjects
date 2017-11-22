c8cd2ce1e9b91f395819c50e08f268b7daba820b
neonblueflame <neonblueflame@gmail.com>
2017-11-23 02:55:03 +0800

    Initial commit for SampleJaxRS

---
+++ SampleJaxRS/src/blueflamey/sample/server/Rest.java
@@ -0,0 +1,32 @@
+package blueflamey.sample.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import blueflamey.sample.service.Calculator;
+
+public class Rest {
+	public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                Calculator.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+    }
+}
